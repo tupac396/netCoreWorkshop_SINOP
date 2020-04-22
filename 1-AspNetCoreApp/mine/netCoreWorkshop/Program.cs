3155f16bb8a4c715417cf2f32af5fd39a2b483f5
Tupac <tupac@etlgr.com>
2020-04-22 19:44:42 -0300

    Primera clase

---
+++ 1-AspNetCoreApp/mine/netCoreWorkshop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace netCoreWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+	        WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+	        
+	
+    }
+}
